6566980ce327c71aad059ae47e2e4c458048fdd1
bennypi <bennypi@tzi.de>
2017-06-18 18:17:44 +0200

    Use actionclient to move arm to different poses

--- scripts/MoveArmServer.py
+++ scripts/MoveArmServer.py
@@ -11,22 +11,81 @@ import robot_assisted_calibration.msg
 
 
 class MoveArmServer(object):
-    # create messages that are used to publish feedback/result
-    feedback = robot_assisted_calibration.msg.MoveArmFeedback()
-    result = robot_assisted_calibration.msg.MoveArmResult()
 
-    def __init__(self, name):
+    def __init__(self, name, caltab_x, caltab_y, caltab_z):
         self.action_name = name
+        self.caltab_position_x = caltab_x
+        self.caltab_position_y = caltab_y
+        self.caltab_position_z = caltab_z
+
+        # Initialize the actionserver
+        self.result = robot_assisted_calibration.msg.MoveArmResult()
         self.action_server = actionlib.SimpleActionServer(self.action_name,
                                                           robot_assisted_calibration.msg.MoveArmAction,
                                                           execute_cb=self.execute_cb, auto_start=False)
         self.action_server.start()
+        rospy.loginfo('MoveArm actionserver started')
+
+        # Initialize the moveit_commander
+        moveit_commander.roscpp_initialize(sys.argv)
+        # robot = moveit_commander.RobotCommander()
+        # scene = moveit_commander.PlanningSceneInterface()
+        self.group = moveit_commander.MoveGroupCommander("manipulator")
 
     def execute_cb(self, goal):
-        rospy.loginfo('Received MoveArmAction with posegoal:' + goal.pose)
+        pose_as_string = ', '.join(str(v) for v in goal.pose)
+        rospy.loginfo('Received MoveArmAction with posegoal: ' + pose_as_string)
+
+        pose = geometry_msgs.msg.Pose()
+        pose.position.x = goal.pose[0]
+        pose.position.y = goal.pose[1]
+        pose.position.z = goal.pose[2]
+
+        self.plan_and_execute(pose)
+        
+        self.result.motion_successful = True
+        self.action_server.set_succeeded(self.result)
+
+    def find_quaternion_for_postion(self, endeffector_position):
+        vector = geometry_msgs.msg.Point()
+        vector.x = self.caltab_position_x - endeffector_position.x
+        vector.y = self.caltab_position_y - endeffector_position.y
+        vector.z = self.caltab_position_z - endeffector_position.z
+
+        yaw = math.atan2(vector.y, vector.x)
+        pitch = -math.asin(vector.z)
+
+        print yaw, pitch
+
+        quaternion = geometry_msgs.msg.Quaternion(*tf_conversions.transformations.quaternion_from_euler(0, pitch, yaw))
+        print quaternion
+        return quaternion
+
+    def plan_and_execute(self, pose):
+        q = self.find_quaternion_for_postion(pose.position)
+        i = geometry_msgs.msg.Quaternion()
+
+        i.x = 0
+        i.y = 0
+        i.z = 0
+        i.w = 1
+
+        pose.orientation = q
+        self.group.set_pose_target(pose)
+        plan = self.group.plan()
+        count = 0
+        while len(plan.joint_trajectory.points) == 0:
+            if count == 4:
+                print 'No plan found after five tries, aborting.'
+                return
+            print 'Could not find plan to goal, replanning...'
+            plan = self.group.plan()
+            count += 1
+        self.group.execute(plan)
+
 
 
 if __name__ == '__main__':
     rospy.init_node('MoveArm')
-    server = MoveArmServer(rospy.get_name)
+    server = MoveArmServer(rospy.get_name(), 1, 0, 0.5)
     rospy.spin()
--- scripts/simple_client.py
+++ scripts/simple_client.py
@@ -7,15 +7,13 @@ import rospy
 import actionlib
 
 from caltab_detector.msg import FindCaltabAction, FindCaltabGoal, CalibrateAction, CalibrateGoal
+from robot_assisted_calibration.msg import MoveArmAction, MoveArmGoal
 
-if __name__ == '__main__':
-    print 'Node started'
-    rospy.init_node('ActionClient')
 
+def caltab_detector_client():
     find_caltab_client = actionlib.SimpleActionClient('FindCaltab', FindCaltabAction)
     find_caltab_client.wait_for_server()
     print 'Waiting for Server finished'
-
     for i in range(0, 10):
         goal = FindCaltabGoal()
         goal.retries = 10
@@ -24,7 +22,6 @@ if __name__ == '__main__':
         find_caltab_client.wait_for_result()
         print find_caltab_client.get_result()
         rospy.sleep(5)
-
     print 'Finished finding caltabs'
     calibrate_client = actionlib.SimpleActionClient('Calibrate', CalibrateAction)
     calibrate_client.wait_for_server()
@@ -35,3 +32,28 @@ if __name__ == '__main__':
     print 'goal sent'
     calibrate_client.wait_for_result()
     print calibrate_client.get_result()
+
+def move_arm_client(poses):
+    move_arm_client = actionlib.SimpleActionClient('MoveArm', MoveArmAction)
+    move_arm_client.wait_for_server()
+    print 'Waiting for Server finished'
+    goal = MoveArmGoal()
+    goal.pose = poses
+    move_arm_client.send_goal(goal)
+    print 'SendGoal'
+    move_arm_client.wait_for_result()
+    print move_arm_client.get_result()
+
+
+
+if __name__ == '__main__':
+    print 'Node started'
+    rospy.init_node('ActionClient')
+
+    # caltab_detector_client()
+    move_arm_client([0.35, 0.5, 0.5])
+    print 'First pose'
+    move_arm_client([0.35, 0, 0.5])
+    print 'Second pose'
+    move_arm_client([0.35, -0.5, 0.5])
+    print 'Third pose'
